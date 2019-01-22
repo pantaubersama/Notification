8e17c637eba7f3f82eb6e5bd0bc4eea730e308db
alamybs <namakukingkong@gmail.com>
2019-01-22 18:38:35 +0700

    Fix typo adn add log in profile.

--- app/services/subscribers/profile.rb
+++ app/services/subscribers/profile.rb
@@ -5,45 +5,60 @@ module Subscribers
 
     def work(params)
       params = json_response params
+      logger.info "Subscribers::Profile"
+      logger.info " --params: #{params}"
+
       if params[:notif_type].eql?("profile") && params[:event_type].eql?("gagal_verifikasi")
+        logger.info " --event_type: gagal_verifikasi"
         ProfileService.new.when_gagal_verifikasi(params[:receiver_id])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("berhasil_verifikasi")
+        logger.info " --event_type: berhasil_verifikasi"
         ProfileService.new.when_berhasil_verifikasi(params[:receiver_id])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("claster_approved")
+        logger.info " --event_type: claster_approved"
         ProfileService.new.when_claster_approved(params[:receiver_id], params[:cluster_id])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("claster_rejected")
+        logger.info " --event_type: claster_rejected"
         ProfileService.new.when_claster_rejected(params[:receiver_id])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("cluster_invited")
+        logger.info " --event_type: cluster_invited"
         ProfileService.new.when_invited_to_claster(params[:receiver_id], params[:user_action_id], params[:cluster_id])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("badge_tanya")
+        logger.info " --event_type: badge_tanya"
         ProfileService.new.when_receive_badge_tanya(params[:receiver_id], params[:badge_title])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("badge_kuis")
+        logger.info " --event_type: badge_kuis"
         ProfileService.new.when_receive_badge_kuis(params[:receiver_id], params[:badge_title])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("badge_lapor")
+        logger.info " --event_type: badge_lapor"
         ProfileService.new.when_receive_badge_lapor(params[:receiver_id], params[:badge_title])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("badge_janji_politik")
-        ProfileService.when_receive_badge_janji_politik(params[:receiver_id], params[:badge_title])
+        logger.info " --event_type: badge_janji_politik"
+        ProfileService.new.when_receive_badge_janji_politik(params[:receiver_id], params[:badge_title])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("badge_tanya_interaksi")
-        ProfileService.when_receive_badge_tanya_interaksi(params[:receiver_id], params[:badge_title])
+        logger.info " --event_type: badge_tanya_interaksi"
+        ProfileService.new.when_receive_badge_tanya_interaksi(params[:receiver_id], params[:badge_title])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("badge_profile")
-        ProfileService.when_receive_badge_profile(params[:receiver_id], params[:badge_title])
+        logger.info " --event_type: badge_profile"
+        ProfileService.new.when_receive_badge_profile(params[:receiver_id], params[:badge_title])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("badge_relawan")
-        ProfileService.when_receive_badge_relawan(params[:receiver_id], params[:badge_title])
+        logger.info " --event_type: badge_relawan"
+        ProfileService.new.when_receive_badge_relawan(params[:receiver_id], params[:badge_title])
 
       elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("badge_pantau_bersama")
-        logger.info "Badge pantau bersama ... #{params}"
-        ProfileService.when_receive_badge_pantau_bersama(params[:receiver_id], params[:badge_title])
+        logger.info " --event_type: badge_pantau_bersama"
+        ProfileService.new.when_receive_badge_pantau_bersama(params[:receiver_id], params[:badge_title])
       end
       ack!
     end
