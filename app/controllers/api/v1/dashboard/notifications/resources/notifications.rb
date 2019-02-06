dc912d8ca74615fa69702686c87226f543a3f85e
myusuf <muhammadyusuf931@gmail.com>
2019-02-06 13:48:04 +0700

    add filter list broadcast

--- app/controllers/api/v1/dashboard/notifications/resources/notifications.rb
+++ app/controllers/api/v1/dashboard/notifications/resources/notifications.rb
@@ -9,10 +9,13 @@ class API::V1::Dashboard::Notifications::Resources::Notifications < API::V1::App
       headers AUTHORIZATION_HEADERS
       detail "list broadcast"
     end
+    params do
+      optional :title, type: String, desc: "Title Notification"
+    end
     paginate per_page: Pagy::VARS[:items], max_per_page: Pagy::VARS[:max_per_page]
     oauth2
     get do
-      resources = paginate(NotificationLog.all)
+      resources = paginate(NotificationLog.where("data like ?", "%#{params.title}%"))
       present :notifications, resources, with: API::V1::Dashboard::Notifications::Entities::Notification
       present_metas resources
     end
