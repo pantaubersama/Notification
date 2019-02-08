5a6a5a7e693c120b471ad45d9d3956ff74d690dc
alamybs <namakukingkong@gmail.com>
2019-02-09 01:07:04 +0700

    Fix badge notif type params fixed.

--- app/services/subscribers/badge.rb
+++ app/services/subscribers/badge.rb
@@ -8,35 +8,35 @@ module Subscribers
       logger.info "Subscribers::Badge"
       logger.info " --params: #{params}"
 
-      if params[:notif_type].eql?("profile") && params[:event_type].eql?("tanya")
+      if params[:notif_type].eql?("badge") && params[:event_type].eql?("tanya")
         logger.info " --event_type: tanya"
         BadgeService.new.when_receive_badge_tanya(params[:receiver_id], params[:badge_id])
 
-      elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("kuis")
+      elsif params[:notif_type].eql?("badge") && params[:event_type].eql?("kuis")
         logger.info " --event_type: kuis"
         BadgeService.new.when_receive_badge_kuis(params[:receiver_id], params[:badge_id])
 
-      elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("lapor")
+      elsif params[:notif_type].eql?("badge") && params[:event_type].eql?("lapor")
         logger.info " --event_type: lapor"
         BadgeService.new.when_receive_badge_lapor(params[:receiver_id], params[:badge_id])
 
-      elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("janji_politik")
+      elsif params[:notif_type].eql?("badge") && params[:event_type].eql?("janji_politik")
         logger.info " --event_type: janji_politik"
         BadgeService.new.when_receive_badge_janji_politik(params[:receiver_id], params[:badge_id])
 
-      elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("tanya_interaksi")
+      elsif params[:notif_type].eql?("badge") && params[:event_type].eql?("tanya_interaksi")
         logger.info " --event_type: tanya_interaksi"
         BadgeService.new.when_receive_badge_tanya_interaksi(params[:receiver_id], params[:badge_id])
 
-      elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("profile")
-        logger.info " --event_type: profile"
-        BadgeService.new.when_receive_badge_profile(params[:receiver_id], params[:badge_id])
+      elsif params[:notif_type].eql?("badge") && params[:event_type].eql?("badge")
+        logger.info " --event_type: badge"
+        BadgeService.new.when_receive_badge_badge(params[:receiver_id], params[:badge_id])
 
-      elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("relawan")
+      elsif params[:notif_type].eql?("badge") && params[:event_type].eql?("relawan")
         logger.info " --event_type: relawan"
         BadgeService.new.when_receive_badge_relawan(params[:receiver_id], params[:badge_id])
 
-      elsif params[:notif_type].eql?("profile") && params[:event_type].eql?("pantau_bersama")
+      elsif params[:notif_type].eql?("badge") && params[:event_type].eql?("pantau_bersama")
         logger.info " --event_type: pantau_bersama"
         BadgeService.new.when_receive_badge_pantau_bersama(params[:receiver_id], params[:badge_id])
       end
