25f329b57e7a4a532409fd54531cab21d89a76d7
alamybs <namakukingkong@gmail.com>
2019-03-02 08:37:46 +0700

    Add avatar in upvote.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < PantauAuthApplicationRecord
   mount_uploader :avatar, AvatarUploader
   has_many :firebase_keys
+
+  def path_avatar
+    {
+      thumbnail:        avatar.thumbnail.url,
+      thumbnail_square: avatar.thumbnail_square.url,
+      medium:           avatar.medium.url,
+      medium_square:    avatar.medium_square.url,
+    }
+  end
 end
